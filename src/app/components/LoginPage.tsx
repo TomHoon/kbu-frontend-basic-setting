4721b40a568dae0b8df0fe468ae268929d0c121e
donghoonlee <gnsdl9108@naver.com>
2026-06-22 23:44:59 +0900

    feat: zustand

---
+++ src/app/components/LoginPage.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import { useEffect } from "react";
+import { useUserStore } from "../store/UserStore"
+
+export default function Login() {
+
+  const setUser = useUserStore(state => state.setUser);
+
+
+  return (
+    <div className="mx-auto max-w-md rounded-lg border bg-white p-6">
+      <h1 className="mb-6 text-center text-2xl font-bold">로그인</h1>
+
+      <div className="space-y-4">
+        <input
+          type="text"
+          placeholder="아이디"
+          className="w-full rounded border px-3 py-2"
+        />
+
+        <input
+          type="password"
+          placeholder="비밀번호"
+          className="w-full rounded border px-3 py-2"
+        />
+
+        <button className="w-full rounded bg-blue-600 py-2 text-white" onClick={() => {
+          setUser({
+            id: 1,
+            name: "lee"
+          });
+        }}>
+          로그인
+        </button>
+      </div>
+    </div>
+  )
+
+}
--- src/app/fetch/client/page.tsx
+++ src/app/fetch/client/page.tsx
@@ -1,6 +1,17 @@
+"use client"
+
 import ClientPostList from '@/app/components/ClientPostList';
+import { useUserStore } from '@/app/store/UserStore';
+import { useEffect } from 'react';
 
 export default function ClientFetchPage() {
+
+
+	const user = useUserStore(state => state.user);
+	useEffect(() => {
+		console.log(user);
+	}, [user])
+
 	return (
 		<div className="rounded-lg border bg-white p-6">
 			<h1 className="mb-4 text-2xl font-bold">Client Component Fetching</h1>
